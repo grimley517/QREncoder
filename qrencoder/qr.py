ba1c328746e83ac42e325ee2fe4e78a7f7f767f2
Brian Jones <ba.jones@me.com>
2014-05-05 13:44:31 -0400

    day 2 - msg type recognition

--- qrencoder/qr.py
+++ qrencoder/qr.py
@@ -1,11 +1,21 @@
 class qrc(object):
     sizemin = 1
     sizemax = 40
+    sizelist = [26,44,70,100,134,172,196,242,292,346,404,466,532, 582, 655,733, 
+    835,901,991,1085,1156,1258,1364, 1474, 1588, 1706, 1828, 1921, 2051, 2185, 
+    2323, 2465, 2611, 2761, 2876, 3034, 3196, 3362, 3532, 3706]
     
-    def __init__ (self, size = 0, msg = '', quality = 'L'):
-        self.size = size
-        self.msg = msg
+    def __init__ (self, msg = '', quality = 'L'):
+        self.msg = str(msg)
         self.quality = quality
+        if self.msg.isdigit():
+            self.msgtype = 0
+            
+        elif self.msg.isalnum():
+            self.msgtype = 1
+        else:
+            raise typeerror
+        
         
     def __repr__(self):
         return self.msg
--- qrencoder/qrtests.py
+++ qrencoder/qrtests.py
@@ -3,11 +3,30 @@ import unittest
 
 class fntests(unittest.TestCase):
     
-    def setUp(self):
-        self.block = qrcode(size = 0, msg = 'hello', quality = 'M')
         
     def test1(self):
+        ''' This test checks that a qrcode object can be made
+        '''
+    
+        self.block = qrcode( msg = 'hello', quality = 'M')
         assert(str(self.block) =='hello') #Test 1 fails - qr object not created, and does not have correct string representation.
     
+    def test2(self):
+        '''This test is to make sure that a block can regognise what type of data it is handling
+        
+        '''
+        msg = 0
+        self.block = qrcode (msg = msg)
+        assert (self.block.msgtype == 0)#test 2 fails, numeric message is not recognised
+    
+    def test3(self):
+        ''' This test is to ensure that an alphanumeric message is recognised
+        
+        '''
+        msg = 'hi'
+        self.block = qrcode (msg = msg)
+        assert (self.block.msgtype == 1)# test 3 fails, alphanumeric message in not recongised
+    
+        
 if __name__ == '__main__':
     unittest.main()
