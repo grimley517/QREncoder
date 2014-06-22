7f388bf482a1ca7a5dcb7a7d22eb9aabfe311944
Brian Jones <ba.jones@me.com>
2014-06-22 14:01:42 -0400

    can now work out ver number for numeric messages

--- qrencoder/qr.py
+++ qrencoder/qr.py
@@ -2,51 +2,67 @@ import math
 class qrc(object):
     sizemin = 1
     sizemax = 40
-    sizeListChar = [26,44,70,100,134,172,196,242,292,346,404,466,532, 582, 655,733, 
-    835,901,991,1085,1156,1258,1364, 1474, 1588, 1706, 1828, 1921, 2051, 2185, 
-    2323, 2465, 2611, 2761, 2876, 3034, 3196, 3362, 3532, 3706]
+    sizeList =[[41,34,27,17],
+        [77,63,48,34],
+        [127, 101, 77,58],
+        [187, 149, 111,82],
+        [255, 202, 144,106],
+        [322, 255, 178,139],
+        [370, 293, 207,194],
+        [461, 365, 259,202],
+        [552, 432, 312,235],
+        [652, 513, 364,288],
+        [772, 604, 427, 331],
+        [883, 691, 489, 371],
+        [1022,796, 580,427],
+        [1101, 871, 621, 468],
+        [1250, 991, 702, 503],
+        [1408, 1082, 775, 602],
+        [1548, 1212, 876, 674],
+        [1725, 1346, 948, 746],
+        [1903, 1500, 1063, 813],
+        [2061, 1600, 1159, 919],
+        [2232, 1708, 1224, 969],
+        [2409, 1872, 1359, 1056],
+        [2620, 2059, 1468, 1108],
+        [2812, 2188, 1588, 1228],
+        [3057, 2395, 1718, 1286],
+        [3283, 2544, 1804, 1425],
+        [3517, 2701, 1933, 1501],
+        [3669, 2857, 2085, 1581],
+        [3909, 3035, 2181, 1677],
+        [4158, 3289, 2358, 1782],
+        [4417, 3486, 2473, 1897],
+        [4686, 3693, 2670, 2022],
+        [4965, 3909, 2805, 2157],
+        [5253, 4134, 2949, 2301],
+        [5529, 4343, 3081, 2361],
+        [5836, 4588, 3244, 2524],
+        [6153, 4775, 3417, 2625],
+        [6479, 5039, 3599, 2735],
+        [6743, 5313, 3791, 2927],
+        [7089, 5596, 3993, 3057]]
     
     def __init__ (self, msg = '', quality = 'L'):
         self.msg = str(msg)
         self.quality = quality
         if self.msg.isdigit():
             self.msgtype = 0
-            self.getSizeNumeric()
+            self.size = self.getSizeNumeric(msg)
         elif self.msg.isalnum():
             self.msgtype = 1
         else:
             raise TypeError('message type not recognised')
     
-    def getSizeNumeric(self):
-        sizeListBin = []
-        for i,n in enumerate(self.sizeListChar, start =1):
-            sizeBin = n*8
-            sizeBin = sizeBin -14
-            if i>9:
-                sizeBin = sizeBin -2
-            if i>26:
-                sizeBin = sizeBin -2
-            sizeListBin.append(sizeBin)
-        D = len(self.msg)
-    
-        
-        R = D%3
-        if R == 1:
-            R = 4
-        elif R==2:
-            R = 7
-        D = math.floor(D/3)
-        D = 10*D
-        bitsRequired = D+R
-        bitsRequired = int(bitsRequired)
-        ver = 1
-        for size in sizeListBin:
-            if bitsRequired > size:
-                ver +=1
-        self.size = ver
-        #if self.size > 40:
-        #    raise ValueError ('Version too Large')
-        
+    def getSizeNumeric(self, message, enc=2):
+        messageLength = len(message)
+        answer = 1
+        for i,n in enumerate(self.sizeList):
+            if messageLength > n[enc]:
+                answer = answer +1
+        if answer > 40:
+            raise ValueError('Size too large for any version')
+        return (answer)
         
     def __repr__(self):
         return self.msg
--- qrencoder/versizetests.py
+++ qrencoder/versizetests.py
@@ -3,19 +3,30 @@ import unittest
 import random
 
 class vstests(unittest.TestCase):
-    
+    def setUp(self):
+        self.letters = list('0123456789')
+        
     def test1(self):
         with open('sizetestData.txt', 'w') as ofile:
-            letters = list('0123456789')
             msg = ''
             self.ver = 0
-            while self.ver<41:
-                msg = msg + random.choice(letters)
+            while self.ver<40:
+                msg = msg + random.choice(self.letters)
                 code = qrcode(msg = msg)
                 if self.ver < code.size:
                     ofile.write('msg of length {0} generates a version {1} code \n'.format(len(msg),code.size))
                     self.ver = code.size
                     
+    def test2(self):
+        code = qrcode('1')
+        assert (code.size ==1)
         
+    def test3(self):
+        msg = ''
+        while len(msg) < 3994:
+            msg = msg + random.choice(self.letters)
+        self.assertRaises(ValueError, qrcode, msg)
+            
+            
 if __name__ == '__main__':
     unittest.main()
